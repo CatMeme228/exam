d502e31972115de3f22c8fb8254eb71ef47e1e09
Cat_meme <39881747+CatMeme228@users.noreply.github.com>
2024-10-11 21:09:50 +0300

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -8,3 +8,15 @@ def change_sys(num, base):
         num=num//base
     return res
 print(change_sys(5,25))
+
+from itertools import *
+
+alph="abcd"
+alph2="efgh"
+res=product(alph, repeat=3)#размещение
+res=permutations(alph)#перестановка
+res= combinations(alph, 2)#сочитания
+res= combinations_with_replacement(alph, 2)#сочитания с повтором
+for i in res:
+    print(''.join(i))
+
