4192445aa57ce2b050f9fbb470871cd36081a9b0
Cat_meme <39881747+CatMeme228@users.noreply.github.com>
2024-10-13 17:31:44 +0300

    Update numer_sys.py

--- numer_sys.py
+++ numer_sys.py
@@ -1,7 +1,12 @@
 def change_sys(num, base):
     res=""
+    alph = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    alph= sorted(alph)
     while num>0:
-        res=str(num%base)+res
+        if num%base>9:
+            res = alph[int(num%base)-10] + res
+        else:
+            res=str(num%base)+res
         num=num//base
     return res
 
