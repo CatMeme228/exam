f077b678286185c19528db257f16f91b759a3f95
Cat_meme <39881747+CatMeme228@users.noreply.github.com>
2024-10-18 19:40:16 +0300

    Update lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -1,7 +1,7 @@
 def change_sys(num, base):
     res=""
     alph = "0123456789qwertyuiopasdfghjklzxcvbnm"
-    alph.sorted
+    alph=sorted(alph)
     while num>0:
         res = alph[int(num%base)] + res
         num=num//base
