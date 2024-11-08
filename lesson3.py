b6181c146902d6ac34e00ca6619c4f24f0c0e8c8
Cat_meme <39881747+CatMeme228@users.noreply.github.com>
2024-11-08 19:11:58 +0300

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -11,3 +11,38 @@ for el in res:
     ind+=1
 #3.21
 print(len([el for el in product('01', repeat=4) if el.count("1")==2]))
+
+# def change_sys(num, base):
+#     res=""
+#     alph = "QWERTYUIOPASDFGHJKLZXCVBNM"
+#     alph= sorted(alph)
+#     while num>0:
+#         if num%base>9:
+#             res = alph[int(num%base)-10] + res
+#         else:
+#             res=str(num%base)+res
+#         num=num//base
+#     return res
+# counter=0
+# for i in range(int("10000000", 6), int("100000000",6)):
+#     if change_sys(i, 6)[0]!="2" or change_sys(i, 6)[0]!="3":
+#         for j in range(len(change_sys(i,6))-2):
+#             count_tmp=0
+#             i_bin=bin(i)[2:]
+#             if i_bin[j]!=i_bin[j+1] and i_bin[j]!=i_bin[j+2] and i_bin[j+1]!=i_bin[j+2]:
+#                 count_tmp+=1
+#             if count_tmp+2==len(change_sys(i,6)):
+#                 counter+=1
+# print(counter)
+
+counter=0
+for i in range(int("10000",9), int("100000",9)):
+    checked=1
+    for j in range(len(str(i))-1):
+        tmp=str(i)
+        if tmp[j]==tmp[j+1] or (int(tmp[j])+int(tmp[j+1]))%3!=0:
+            checked=0
+    if i%10!=9 and checked==1 and str(i).count("6")!=0 and str(i).count("9")==0:
+        print(i)
+        counter+=1
+print(counter)
