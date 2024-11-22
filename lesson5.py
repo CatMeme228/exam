d84017fe334352f0c34dffb32ae112c11533d32c
Cat_meme <39881747+CatMeme228@users.noreply.github.com>
2024-11-22 19:11:44 +0300

    Create lesson5.py

---
+++ lesson5.py
@@ -0,0 +1,12 @@
+# from itertools import product
+# alph="ПИРОГ"
+# counter=0
+# for word in product(alph, repeat=5):
+#     tmp_count=0
+#     for letter in word:
+#         if letter=="П":
+#             tmp_count+=1
+#     if tmp_count==2:
+#         counter+=1
+# print(counter)
+print(len(bin(60)[2:]))
