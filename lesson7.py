98c78522c546c55b500adf67aaf7ed0b1c4b2ef2
Cat_meme <39881747+CatMeme228@users.noreply.github.com>
2024-11-29 20:19:58 +0300

    Update lesson7.py

--- lesson7.py
+++ lesson7.py
@@ -47,17 +47,40 @@
 #                 print(p)
 
 #7.22
-from itertools import *
-def f(x,y,z,w):
-    return ((w<=y)<=x)or not z
+# from itertools import *
+# def f(x,y,z,w):
+#     return ((w<=y)<=x)or not z
+#
+# for val in product([0,1], repeat=7):
+#     table=[
+#         (val[0],val[1],1,val[2]),
+#         (val[3],0,val[4],val[5]),
+#         (val[6],1,0,0),
+#     ]
+#     if len(table)== len(set(table)):
+#         for p in permutations('xyzw'):
+#             if [f(**dict(zip(p,row))) for row in table]== [0,0,0]:
+#                 print(p)
 
-for val in product([0,1], repeat=7):
-    table=[
-        (val[0],val[1],1,val[2]),
-        (val[3],0,val[4],val[5]),
-        (val[6],1,0,0),
-    ]
-    if len(table)== len(set(table)):
-        for p in permutations('xyzw'):
-            if [f(**dict(zip(p,row))) for row in table]== [0,0,0]:
-                print(p)
+#7.12
+def f(x,y,z):
+    return (x and (not y) or z) and (y and z or (not y))
+
+from itertools import permutations
+
+table_0=[
+    [0,0,0],
+    [0,0,1],
+    [0,1,1]
+]
+
+table_1=[
+    [0,1,0],
+    [1,0,0],
+    [1,0,1],
+    [1,1,0],
+    [1,1,1]
+]
+for p in permutations("xyz"):
+    if [f(**dict(zip(p, row_0))) for row_0 in table_0]==[0,0,0] and [f(**dict(zip(p, row_1))) for row_1 in table_1]==[1,1,1]:
+        print(p)
